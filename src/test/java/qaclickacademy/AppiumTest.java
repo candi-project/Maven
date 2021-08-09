b0d57839ab6e78cb6be1e5c2cb96618dcfe79048
Candi615 <cchiu615@gmail.com>
2021-08-09 23:22:10 +0800

    MavenEssential

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void MobileAutomation()
+	{
+		System.out.println("MobileAutomation");
+	}
+	
+	@Test
+	public void MobileLogin()
+	{
+		System.out.println("MobileLogin");
+	}
+	
+}
